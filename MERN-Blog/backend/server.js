a2dab57725ae9448b5e6cc15c98761088deaf391
ShivamVarshney <varshneyshivam@gmail.com>
2026-04-14 13:12:53 +0530

    completes

--- MERN-Blog/backend/server.js
+++ MERN-Blog/backend/server.js
@@ -39,7 +39,7 @@ app.use("/api/v1/comment", commentRoute);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(
       path.join(__dirname, "../frontend/dist/index.html")
     );
